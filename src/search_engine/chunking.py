a66580080e48cc9a33e464b13ff997d7efa48a6a
Aayush <168896919+gumnaam4@users.noreply.github.com>
2026-02-18 15:28:55 +0530

    Update chunking.py

--- src/search_engine/chunking.py
+++ src/search_engine/chunking.py
@@ -1,14 +1,13 @@
-def chunk_documents(documents):
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 
+def chunk_documents(documents):
     chunks = []
 
     for d in documents:
-
-        text = d["text"]
-
-        split = [text[:30], text[30:]]
-
-        for s in split:
-            chunks.append({"id":d["id"],"text":s})
+        sentences = sent_tokenize(d["text"])
+        for s in sentences:
+            chunks.append({"id": d["id"], "text": s})
 
     return chunks
