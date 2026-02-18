f4c747bc0a591089ec8066768652199f29cf8ec7
Aayush <168896919+gumnaam4@users.noreply.github.com>
2026-02-18 15:30:21 +0530

    Update retrieval.py

--- src/search_engine/retrieval.py
+++ src/search_engine/retrieval.py
@@ -1,9 +1,7 @@
-import numpy as np
-
 def retrieve(chunks, scores, top_k=3):
 
     indexed = list(zip(chunks, scores))
 
-    indexed.sort(key=lambda x: x[1])
+    indexed.sort(key=lambda x: x[1], reverse=True)
 
     return [i[0] for i in indexed[:top_k]]
